558feca3d6428ce66bb11dc444b1ac1e3bae917c
Lucas Bäckman <lucas.backman@gmail.com>
2022-05-18 16:05:27 +0300

    Create class Game, and method play with pseudocode

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
+# Hangman game
+class Game
+  def initialize(word)
+    @word = word
+    @letters_guessed = []
+    @letters_wrong = []
+    @letters_correct = []
+    @wrong_guess_count = 0
+  end
+
+  def play
+    # Have player guess a word by giving a letter
+    # If word.inlcude? letter => reveal letter in word
+    # and add letter to the letters_correct array
+    # If not, increase wrong_guess_count
+    # and add letter to the letters_wrong array
+    # Unless win before wrong_guess_count reaches N (TBD)
+    # Then lose
+    # Else win
+  end
+end
+
 def get_words(textfile)
   File.read(textfile).lines.map(&:chomp).select { |w| correct_length?(w) }
 end
