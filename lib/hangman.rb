6f84731d5ab46b32f6f90135ac65be6e7307ad76
Lucas Bäckman <lucas.backman@gmail.com>
2022-05-18 14:48:22 +0300

    Select words of certain length

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,12 @@
-words = File.read('wordlist.txt').lines.map(&:chomp)
-puts words.first
-puts words.last
+# frozen_string_literal: true
+
+def get_words(textfile)
+  File.read(textfile).lines.map(&:chomp).select { |w| correct_length?(w) }
+end
+
+def correct_length?(word)
+  word.length >= 5 && word.length <= 12
+end
+
+words = get_words('wordlist.txt')
+10.times { puts words.sample }
